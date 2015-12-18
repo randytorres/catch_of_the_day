e1f1567a22b29447a36069bc8aa2d8e0e3e4a6a9
Randy Torres <torresr14@gmail.com>
2015-12-18 00:33:31 -0800

    First Component created

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+/* StorePicker
+    This will let us make <StorePicker>
+    */
+var StorePicker = React.createClass({
+
+  render : function () {
+    return (
+      <p>Hi</p>
+    )
+  }
+});
+
+  ReactDOM.render(<StorePicker/>, document.querySelector("#main"));
