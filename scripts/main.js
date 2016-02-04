43a6c552b12c5824d8723110a9e9cbc4d00c252e
Randy Torres <torresr14@gmail.com>
2016-02-03 20:21:24 -0800

    Added Firebase

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,10 @@ var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers');
 
+// Firebase
+var Rebase = require('re-base');
+var base = Rebase.createClass('https://catchoftheday23.firebaseio.com/');
+
 /*
  App
  */
@@ -19,6 +23,24 @@ var App = React.createClass({
       fishes : {},
       order : {}
     }
+  },
+  componentDidMount() {
+    base.syncState(this.props.params.storeId + '/fishes', {
+      context : this,
+      state : 'fishes'
+    });
+    var localStorageRef = localStorage.getItem('order-' + this.props.
+    params.storeId);
+
+    if(localStorageRef) {
+      this.setState({
+        order : JSON.parse(localStorageRef)
+      });
+    }
+  },
+
+  componentWillUpdate : function(nextProps, nextState) {
+    localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
   },
   addToOrder : function(key) {
     this.state.order[key] = this.state.order[key] + 1 || 1;
@@ -157,7 +179,7 @@ var Order = React.createClass({
     }
 
     return (
-      <li>
+      <li key={key}>
         {count}lbs
         {fish.name}
         <span className="price">{h.formatPrice(count * fish.price)}</span>
@@ -211,7 +233,7 @@ var Inventory = React.createClass({
 })
 
 
-/*
+/* 
  StorePicker
  This will let us make <StorePicker/>
  */
